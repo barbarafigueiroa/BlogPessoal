0aa937c0bc02838c6477775101c543abeaf60266
barbara figueiroa <bfamorim@outlook.com>
2021-12-06 21:51:21 -0300

    configura cadastro e login | cria inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +19,14 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     LoginComponent,
     MenuComponent,
     RodapeComponent,
-    CadastroComponent
+    CadastroComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
